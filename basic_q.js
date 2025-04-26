55107dae3e9bea4baa65d8610b899a8c25f1d106
Krrishbalana <krrishbalana@gmail.com>
2025-04-26 21:57:05 +0530

    basic practice question of multiplication

---
+++ basic_q.js
@@ -0,0 +1,8 @@
+// 1. Create two variables, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge 
+// 3. Log myDogAge to the console
+
+let myAge = 70
+let humanDogRatio = 10
+let myDogAge = myAge * humanDogRatio
+console.log(myDogAge)
