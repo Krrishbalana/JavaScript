d9eb8a9077562bc75f56fee10bbe1b0f99433597
Krrishbalana <krrishbalana@gmail.com>
2025-04-27 17:45:59 +0530

    adding increment function to increcre the person count on every click

--- countomer count/index.js
+++ countomer count/index.js
@@ -3,6 +3,14 @@
 // increment the count variable when the button is clicked
 // change the count-el in the HTML to reflect the new count
 
+// document.getElementById("count-el").innerText = 1
+
+
+let countEl = document.getElementById("count-el")
+
+let count = 0
+
 function increment() {
-    console.log("button triggerd")
+    count = count + 1
+    countEl.innerText = count
 }
