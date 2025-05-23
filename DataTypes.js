a1b7e5606b03ec430a4ec0e3b823028d9a12c5be
Krish Balana <168128787+Krrishbalana@users.noreply.github.com>
2025-05-23 12:55:12 +0530

    Data Types in JavaScript

---
+++ DataTypes.js
@@ -0,0 +1,29 @@
+//there are two type of data- premitive and non-premitive
+// premitive = number, string, boolean, undefined, BIG_int, etc.
+
+// non-premitive = objects;
+
+ const student = {
+     fullName : "krish Balana",
+     age : 22,
+     isPass : true,
+     cgpa : 6.3,
+ };
+
+ student["age"] = student["age"] + 1; // addition in objects;
+
+ const product = {
+     productName : "pen",
+     rating : 4.2,
+     price : 240,
+     discount : 5,
+ }
+
+ const instaProfile = {
+     userName : "@Krrishbalana",
+     isFollow : true,
+     followers : 364,
+     following : 360,
+     bio : "hi I'm Krish nice to see you here",
+
+ };
