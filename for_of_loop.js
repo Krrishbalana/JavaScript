c013ba1398e2ed24fecccaf1b7983b85f213aea1
Krish Balana <168128787+Krrishbalana@users.noreply.github.com>
2025-05-26 16:04:45 +0530

    for_of_loop.js

---
+++ for_of_loop.js
@@ -0,0 +1,10 @@
+//for-of loop ----> basically applied on  string and arrays
+
+let str = "Krishbalana";
+let sizeOfString = 0;
+for(let i of str){
+    console.log("i =", i);
+    sizeOfString++;
+}
+
+console.log("sizeOfString = ", sizeOfString);
