1948d27bf57aa154968a21813ff291a34e8c4915
Krish Balana <168128787+Krrishbalana@users.noreply.github.com>
2025-05-26 15:41:17 +0530

    loops.js

---
+++ loops.js
@@ -0,0 +1,21 @@
+//------------------------------------------------------Loops----------------------------------------------------------------
+// loops are used to execute a piece of code again and again
+
+//1.for loop
+for(let i = 1; i <= 5; i++){
+    console.log(i ,"krish ");
+}
+
+//2.While loop
+let i = 1;
+while(i <= 5){
+ console.log(i);
+ i++;
+}
+
+//3.Do-While loop --> even the condition is false it will run for first time
+let a = 1;
+do{
+    console.log(a);
+    a++;
+}while(a < 0);
